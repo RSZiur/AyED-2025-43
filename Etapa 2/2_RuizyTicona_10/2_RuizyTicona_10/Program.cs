017f0653435aa66884489d75c818a990cb506a52
RSZiur <rubiruiz256@gmail.com>
2025-06-12 10:40:47 -0300

    Add files via upload

---
+++ Etapa 2/2_RuizyTicona_10/2_RuizyTicona_10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_RuizyTicona_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f1, c1;
+            f1 = 5;
+            c1 = 5;
+            int[,] matriz1 = new int[f1, c1];
+            int[,] matriz2 = new int[f1, c1];
+            int[,] matrizsum = new int[f1, c1];
+            Random rand = new Random();
+            for (int i = 0; i < f1; i++)
+            {
+                for (int j = 0; j < c1; j++)
+                {
+                    matriz1[i, j] = rand.Next(1, 45);
+                    matriz2[i, j] = rand.Next(1, 45);
+                    matrizsum[i, j] = matriz1[i, j] + matriz2[i, j];
+                    Console.Write(matrizsum[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 2/2_RuizyTicona_11/2_RuizyTicona_11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_RuizyTicona_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] natural = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+            int[,] rotada = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    rotada[j, 2 - i] = natural[i, j];
+                }
+            }
+            Console.WriteLine("Matriz original:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(natural[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Matriz rotada 90° en sentido horario:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(rotada[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
