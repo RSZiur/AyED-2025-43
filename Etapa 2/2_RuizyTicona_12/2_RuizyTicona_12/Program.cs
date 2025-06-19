65e2032b7380ffe872af9f66d76925c0f249e9a3
RSZiur <rubiruiz256@gmail.com>
2025-06-19 09:19:39 -0300

    Add files via upload

---
+++ Etapa 2/2_RuizyTicona_12/2_RuizyTicona_12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace _2_RuizyTicona_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int Ax = 4;
+            int[] vec = new int[Ax];
+        
+            int[,] Matriz = new int[Ax, Ax];
+            for (int i = 0; i < Ax; i++)
+            {
+                for (int j = 0; j < Ax; j++)
+                {
+                    int num = rand.Next(1, 25);
+                    Matriz[i, j] = num;
+                }
+            }
+
+            Console.WriteLine("Matriz:");
+            for (int i = 0; i < Ax; i++)
+            {
+                for (int j = 0; j < Ax; j++)
+                {
+                    Console.Write(Matriz[i, j] + "\t"); 
+                }
+                Console.WriteLine(); 
+            }
+
+            Console.WriteLine("la diagonal es:");
+            for (int i = 0; i < Ax ; i++)
+            {
+                vec[i] = Matriz[i, i];
+                Console.WriteLine(vec[i]);
+                Console.Write(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
